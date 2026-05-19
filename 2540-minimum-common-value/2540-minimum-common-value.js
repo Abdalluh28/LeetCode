91f0e1eece1d836d6f4620f04d9ad0ac56077dca
Abdalluh28 <99888044+Abdalluh28@users.noreply.github.com>
2026-05-19 13:44:55 +0300

    Time: 1 ms (91.83%), Space: 63.2 MB (58.17%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+    let n = nums1.length, m = nums2.length;
+    for (let i = 0, j = 0; i < n && j < m;) {
+        if (nums1[i] === nums2[j]) {
+            return nums1[i]
+        } else if (nums1[i] < nums2[j]) {
+            i++
+        } else {
+            j++
+        }
+    }
+    return -1
+};
