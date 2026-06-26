06204aed52c3bd56bd2f9042b4da6d3604d79cc5
Abdalluh28 <99888044+Abdalluh28@users.noreply.github.com>
2026-06-26 12:33:25 +0300

    Time: 4 ms (50.43%), Space: 56.9 MB (25.71%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,22 +2,23 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    let arr = [];
-    let temp;
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(' || s[i] === '{' || s[i] === '[' ) {
-            arr.push(s[i]);
+var isValid = function (s) {
+    let stack = []
+    for (let ch of s) {
+        if (ch === '(' || ch === '[' || ch === '{') {
+            stack.push(ch)
         } else {
-            if (arr.length === 0) return false
-            temp = arr.pop();
-            if ( (temp === '(' && s[i] === ')') || (temp === '{' && s[i] === '}') || (temp === '[' && s[i] === ']')  ) {
-
-            } else {
-                return false
+            if (ch === ')') {
+                if (stack.pop() !== '(') return false
+            }
+            if (ch === ']') {
+                if (stack.pop() !== '[') return false
+            }
+            if (ch === '}') {
+                if (stack.pop() !== '{') return false
             }
         }
     }
-    if (arr.length > 0) return false;
+    if (stack.length>0) return false
     return true;
 };
